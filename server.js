1f7e32106bdcbcfed1d6c980aa63b793af56a928
sarahejaz <sarahejazca@gmail.com>
2022-02-24 22:35:13 +0500

    Initialized Sequelize and synced it

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require('./models');
+db.sequelize.sync({ force: true }).then(() => {
+  console.log('Drop and re-sync db.');
+});
+
 app.get('/', (req, res) => {
   res.json({ message: 'Home page' });
 });
