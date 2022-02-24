eda54bf1d4e486eb6e05e55f445b326b6605b306
sarahejaz <sarahejazca@gmail.com>
2022-02-24 23:26:16 +0500

    Added associations

--- models/index.js
+++ models/index.js
@@ -5,8 +5,25 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   dialect: dbConfig.dialect,
   operatorsAliases: false,
 });
+
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+
+db.users = require('./user.model.js')(sequelize, Sequelize);
+db.products = require('./product.model.js')(sequelize, Sequelize);
+db.orders = require('./order.model.js')(sequelize, Sequelize);
+
+db.users.hasMany(db.orders, { as: 'orders' });
+db.orders.belongsTo(db.users, {
+  foreignKey: 'userId',
+  as: 'user',
+});
+
+db.orders.hasMany(db.products, { as: 'products' });
+db.products.belongsTo(db.orders, {
+  foreignKey: 'orderId',
+  as: 'order',
+});
 
 module.exports = db;
