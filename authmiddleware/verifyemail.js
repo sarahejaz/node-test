ae70785da7a19b08966584f3c7f7391ccc5eb4ae
sarahejaz <sarahejazca@gmail.com>
2022-03-17 19:48:27 +0500

    Added next call to auth middleware

--- authmiddleware/verifyemail.js
+++ authmiddleware/verifyemail.js
@@ -1,23 +1,19 @@
 const db = require('../models');
 const User = db.users;
 
-exports.verifyEmail = (req, res) => {
+exports.verifyEmail = (req, res, next) => {
   const email = req.body.email;
   User.findOne({
     where: {
       email: email,
     },
-  })
-    .then((userdata) => {
-      if (userdata) {
-        return res.status(500).send({
-          message: 'Email is already in use',
-        });
-      }
-    })
-    .catch((err) => {
-      return res.send({
-        message: 'Error: ' + err.message,
+  }).then((userdata) => {
+    if (userdata) {
+      res.status(400).send({
+        message: 'Email is already in use',
       });
-    });
+      return;
+    }
+    next();
+  });
 };
